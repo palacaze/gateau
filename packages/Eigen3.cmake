91413ac038f61221237ca6ef8f9f53e4d0fa02b4
Pierre-Antoine Lacaze <pa.lacaze@gmail.com>
2020-03-06 23:30:03 +0100

    Add Eigen3 package

---
+++ packages/Eigen3.cmake
@@ -0,0 +1,7 @@
+set(Eigen3_VERSION "3.3.7")
+set(Eigen3_MD5 "05b1f7511c93980c385ebe11bd3c93fa")
+set(Eigen3_URL "http://bitbucket.org/eigen/eigen/get/${Eigen3_VERSION}.tar.bz2")
+
+macro(Eigen3_find name)
+    find_package(${name} CONFIG ${ARGN})
+endmacro()
