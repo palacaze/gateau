1787efeb3691860b4b7dc516828de3379758aedb
Pierre-Antoine Lacaze <pa.lacaze@gmail.com>
2020-03-06 23:32:25 +0100

    add nlohmann json package

---
+++ packages/nlohmann.cmake
@@ -0,0 +1,4 @@
+set(nlohmann_VERSION "3.7.3")
+set(nlohmann_MD5 "39b754f6834e64406d7eae9dfb9e5d9e")
+set(nlohmann_SINGLE_HEADER "https://github.com/nlohmann/json/releases/download/v${nlohmann_VERSION}/json.hpp")
+
