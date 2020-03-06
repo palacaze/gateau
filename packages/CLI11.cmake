06a3dfbc9bb1d485594ef6988f97001b67d990b2
Pierre-Antoine Lacaze <pa.lacaze@gmail.com>
2020-03-06 23:29:51 +0100

    add CLI11 package

---
+++ packages/CLI11.cmake
@@ -0,0 +1,3 @@
+set(CLI11_VERSION "1.9.0")
+set(CLI11_MD5 "2f801cdeec12e5989ae7247cac173e01")
+set(CLI11_SINGLE_HEADER "https://github.com/CLIUtils/CLI11/releases/download/v${CLI11_VERSION}/CLI11.hpp")
