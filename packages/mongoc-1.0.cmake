168e8730c82fbd6d2610d774a9acdb896059c872
Pierre-Antoine Lacaze <pa.lacaze@gmail.com>
2020-03-06 23:32:08 +0100

    Add mongoc-1.0 package

---
+++ packages/mongoc-1.0.cmake
@@ -0,0 +1,14 @@
+set(mongoc-1.0_URL "https://github.com/mongodb/mongo-c-driver/releases/download/1.16.0/mongo-c-driver-1.16.0.tar.gz")
+set(mongoc-1.0_MD5 "b7af904b3ac094667d4c176c93925bfe")
+set(mongoc-1.0_CMAKE_ARGS
+    -DBUILD_SHARED_LIBS=OFF
+    -DENABLE_EXAMPLES=OFF
+    -DENABLE_ZSTD=OFF
+    -DENABLE_ICU=OFF
+    -DENABLE_BSON=ON
+    -DENABLE_MONGOC=ON
+    -DENABLE_SASL=OFF
+    -DENABLE_STATIC=ON
+    -DENABLE_TESTS=OFF
+    -DENABLE_TRACING=OFF
+    -DENABLE_UNINSTALL=OFF)
