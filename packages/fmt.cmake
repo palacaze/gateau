0fa883778162e4a028c02add9ece7805124f70cf
Pierre-Antoine Lacaze <pa.lacaze@gmail.com>
2020-03-06 23:29:27 +0100

    update fmt package

--- packages/fmt.cmake
+++ packages/fmt.cmake
@@ -1,7 +1,7 @@
-set(fmt_version "5.1.0")
-set(fmt_URL "https://github.com/fmtlib/fmt/archive/${fmt_version}.tar.gz")
-set(fmt_MD5 "89863cfec1448aec409a2eecf62600a2")
+set(fmt_VERSION "6.1.2")
+set(fmt_URL "https://github.com/fmtlib/fmt/archive/${fmt_VERSION}.tar.gz")
+set(fmt_MD5 "2914e3ac33595103d6b27c87364b034f")
 set(fmt_CMAKE_ARGS
-    "-DFMT_DOC=OFF"
-    "-DFMT_TEST=OFF"
+    -DFMT_DOC=OFF
+    -DFMT_TEST=OFF
 )
