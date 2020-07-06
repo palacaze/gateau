8fbe4e3ae6579ce90c6d54ec355dc9b08c3bb0ad
Pierre-Antoine Lacaze <pa.lacaze@gmail.com>
2020-07-06 23:03:10 +0200

    Modernize the Qt5 package

--- packages/Qt5.cmake
+++ packages/Qt5.cmake
@@ -3,19 +3,18 @@ macro(Qt5_find name)
 
     # We add some definitions that make Qt stricter
     if (TARGET Qt5::Core)
-        set_property(TARGET Qt5::Core PROPERTY
-            INTERFACE_COMPILE_DEFINITIONS
-                QT_NO_CAST_FROM_BYTEARRAY
-                QT_NO_CAST_FROM_ASCII
-                QT_NO_CAST_TO_ASCII
-                QT_NO_URL_CAST_FROM_STRING
-                QT_STRICT_ITERATORS
-                QBAL=QByteArrayLiteral
-                QSL=QStringLiteral
-                QL1S=QLatin1String
-                QL1C=QLatin1Char
-                QT_FORCE_ASSERTS
-            APPEND
+        target_compile_definitions(Qt5::Core INTERFACE
+            QT_NO_CAST_FROM_BYTEARRAY
+            QT_NO_CAST_FROM_ASCII
+            QT_NO_CAST_TO_ASCII
+            QT_NO_URL_CAST_FROM_STRING
+            QT_STRICT_ITERATORS
+            QBAL=QByteArrayLiteral
+            QSL=QStringLiteral
+            QL1S=QLatin1String
+            QL1C=QLatin1Char
+            QT_FORCE_ASSERTS
         )
+
     endif()
 endmacro()
