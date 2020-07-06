129c2e7f02ebc13318926188ba67814e1e7ffff4
Pierre-Antoine Lacaze <pa.lacaze@gmail.com>
2020-07-06 23:04:43 +0200

    Fix the abseil package

--- packages/Abseil.cmake
+++
@@ -1,2 +0,0 @@
-# "live at head" they said
-set(Abseil_GIT "https://github.com/abseil/abseil-cpp.git")
---
+++ packages/absl.cmake
@@ -0,0 +1,2 @@
+# "live at head" they said
+set(absl_GIT "https://github.com/abseil/abseil-cpp.git")
