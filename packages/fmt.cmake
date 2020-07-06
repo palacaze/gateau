78eb32a79e473d35e4ff11ab04a45e7ae2243c81
Pierre-Antoine Lacaze <pa.lacaze@gmail.com>
2020-07-06 23:04:00 +0200

    Update the fmt package

--- packages/fmt.cmake
+++ packages/fmt.cmake
@@ -1,6 +1,6 @@
-set(fmt_VERSION "6.1.2")
+set(fmt_VERSION "7.0.0")
 set(fmt_URL "https://github.com/fmtlib/fmt/archive/${fmt_VERSION}.tar.gz")
-set(fmt_MD5 "2914e3ac33595103d6b27c87364b034f")
+set(fmt_MD5 "370757a0bae552679b6481d24d768ece")
 set(fmt_CMAKE_ARGS
     -DFMT_DOC=OFF
     -DFMT_TEST=OFF
