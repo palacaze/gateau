008404b3db8e9ef2a2b051ea10b9656ebf2571a4
Pierre-Antoine Lacaze <pa.lacaze@gmail.com>
2020-07-20 15:44:25 +0200

    package CLI11: update to 1.9.1

--- packages/CLI11.cmake
+++ packages/CLI11.cmake
@@ -1,3 +1,3 @@
-set(CLI11_VERSION "1.9.0")
-set(CLI11_MD5 "2f801cdeec12e5989ae7247cac173e01")
+set(CLI11_VERSION "1.9.1")
+set(CLI11_MD5 "05de518a9321e3b6393fe574ac72c6e8")
 set(CLI11_SINGLE_HEADER "https://github.com/CLIUtils/CLI11/releases/download/v${CLI11_VERSION}/CLI11.hpp")
