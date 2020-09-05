55865dbebe20d31f9a9e625281b5ca6b312a1be4
Pierre-Antoine Lacaze <pa.lacaze@gmail.com>
2020-09-06 00:10:55 +0200

    Add asio package

---
+++ packages/asio.cmake
@@ -0,0 +1,41 @@
+set(asio_GIT "https://github.com/chriskohlhoff/asio.git")
+set(asio_TAG "asio-1-18-0")
+
+macro(asio_find name)
+    include(FindPackageHandleStandardArgs)
+
+    find_path(asio_INCLUDE_DIR
+        NAMES io_service.hpp
+        PATH_SUFFIXES asio
+    )
+
+    find_package_handle_standard_args(
+        asio DEFAULT_MSG asio_INCLUDE_DIR)
+
+    mark_as_advanced(asio_INCLUDE_DIR)
+
+    if(asio_FOUND AND NOT TARGET asio::asio)
+        add_library(asio::asio INTERFACE IMPORTED)
+        set_target_properties(asio::asio PROPERTIES
+            INTERFACE_INCLUDE_DIRECTORIES "${asio_INCLUDE_DIR}"
+        )
+        target_compile_definitions(asio::asio INTERFACE
+            BOOST_ASIO_NO_DEPRECATED
+            BOOST_ASIO_STANDALONE
+            BOOST_ASIO_HEADER_ONLY
+        )
+    endif()
+endmacro()
+
+macro(asio_install name)
+    # call to external project, which installs the dep
+    gateau_install_dependency(${name}
+        NO_PATCH
+        NO_CONFIGURE
+        NO_BUILD
+        INSTALL_COMMAND ${CMAKE_COMMAND} -E make_directory "<INSTALL_DIR>/include/asio"
+                COMMAND ${CMAKE_COMMAND} -E copy_directory "<SOURCE_DIR>/asio/include/asio" "<INSTALL_DIR>/include/asio"
+                COMMAND ${CMAKE_COMMAND} -E copy "<SOURCE_DIR>/asio/include/asio.hpp" "<INSTALL_DIR>/include"
+    )
+endmacro()
+
