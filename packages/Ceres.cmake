6599820bbc331194fd7255c8a0caf153c30e04eb
Pierre-Antoine Lacaze <pa.lacaze@gmail.com>
2020-10-28 13:38:02 +0100

    Add Ceres Solver package

---
+++ packages/Ceres.cmake
@@ -0,0 +1,17 @@
+# Ceres Solver
+set(Ceres_version "2.0.0")
+set(Ceres_URL "https://github.com/ceres-solver/ceres-solver/archive/${Ceres_version}.tar.gz")
+set(Ceres_MD5 "94246057ac520313e3b582c45a30db6e")
+set(Ceres_CMAKE_ARGS
+    -DBUILD_EXAMPLES=OFF
+    -DBUILD_TESTING=OFF
+    -DBUILD_DOCUMENTATION=OFF
+    -DBUILD_BENCHMARKS=OFF
+    -DEIGENSPARSE=ON
+    -DMINIGLOG=OFF
+    -DGFLAGS=OFF
+    -DLAPACK=OFF
+    -DCXSPARSE=OFF
+    -DSUITESPARSE=OFF
+)
+
