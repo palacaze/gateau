de507096f72e0c9ec01ceaeef568b0b6e8acfc88
Pierre-Antoine Lacaze <pa.lacaze@gmail.com>
2021-04-26 10:47:20 +0200

    Fix pybind11 url

--- packages/pybind11.cmake
+++ packages/pybind11.cmake
@@ -1,4 +1,4 @@
-set(pybind11_GIT "https://gitlab.com/pybind11/pybind11")
+set(pybind11_GIT "https://github.com/pybind/pybind11")
 set(pybind11_TAG "master")
 set(pybind11_CMAKE_ARGS
     -DPYBIND11_TEST=OFF
