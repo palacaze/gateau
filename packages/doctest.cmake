167cb3a68b59d4fe5e9f238e0f7c07b95a4e41ac
Pierre-Antoine Lacaze <pa.lacaze@gmail.com>
2021-12-16 16:49:14 +0100

    anchor doctest to a working version

--- packages/doctest.cmake
+++ packages/doctest.cmake
@@ -1,5 +1,5 @@
 set(doctest_GIT "https://github.com/onqtam/doctest.git")
-set(doctest_TAG "master")
+set(doctest_TAG "4d8716f1efc1d14aa736ef52ee727bd4204f4c40")
 set(doctest_CMAKE_ARGS
     -DDOCTEST_WITH_TESTS=OFF
     -DDOCTEST_NO_INSTALL=OFF
