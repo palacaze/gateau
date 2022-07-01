cf8c1eeddd0d549bc6cf02607ba7a8e0c9cfceb5
Pierre-Antoine Lacaze <pal.dev@pa-l.fr>
2022-07-01 21:23:30 +0200

    Add nanoflann package

---
+++ packages/nanoflann.cmake
@@ -0,0 +1,7 @@
+set(nanoflann_VERSION "1.4.2")
+set(nanoflann_URL "https://github.com/jlblancoc/nanoflann/archive/refs/tags/v${nanoflann_VERSION}.tar.gz")
+set(nanoflann_MD5 "90a95679ce797efa315e76dcb79c114d")
+set(nanoflann_CMAKE_ARGS
+    -DNANOFLANN_BUILD_EXAMPLES=OFF
+    -DNANOFLANN_BUILD_TESTS=OFF
+)
