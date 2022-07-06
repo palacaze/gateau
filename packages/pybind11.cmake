6521f8e0c7858b1ee790902852f4f9f778f81db3
Pierre-Antoine Lacaze <pal.dev@pa-l.fr>
2022-07-06 11:58:58 +0200

    Fix pybind11 version

--- packages/pybind11.cmake
+++ packages/pybind11.cmake
@@ -1,5 +1,7 @@
-set(pybind11_GIT "https://github.com/pybind/pybind11")
-set(pybind11_TAG "master")
+# set(pybind11_GIT "https://github.com/pybind/pybind11")
+# set(pybind11_TAG "master")
+set(pybind11_URL "https://github.com/pybind/pybind11/archive/refs/tags/v2.9.2.tar.gz")
+set(pybind11_MD5 "06e4b6c2d0a5d6c6025941203cfcd4b6")
 set(pybind11_CMAKE_ARGS
     -DPYBIND11_TEST=OFF
     -DPYBIND11_NOPYTHON=ON
