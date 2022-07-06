c1d5825ac29028427e9610d8c6fdeaebf031facb
Pierre-Antoine Lacaze <pal.dev@pa-l.fr>
2022-07-06 11:58:30 +0200

    Update nlohmann json package

--- packages/nlohmann.cmake
+++ packages/nlohmann.cmake
@@ -1,4 +1,4 @@
-set(nlohmann_VERSION "3.9.1")
-set(nlohmann_MD5 "5eabadfb8cf8fe1bf0811535c65f027f")
+set(nlohmann_VERSION "3.10.5")
+set(nlohmann_MD5 "f62f0c62d5504e587f57acb4eb3cea30")
 set(nlohmann_SINGLE_HEADER "https://github.com/nlohmann/json/releases/download/v${nlohmann_VERSION}/json.hpp")
 
