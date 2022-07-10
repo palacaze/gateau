7b3561bb219f7ec81077c2c78c471ea9246de17a
Pierre-Antoine Lacaze <pal.dev@pa-l.fr>
2022-07-10 23:34:44 +0200

    fmt package: update to v9

--- packages/fmt.cmake
+++ packages/fmt.cmake
@@ -1,11 +1,9 @@
-#set(fmt_VERSION "7.0.1")
-#set(fmt_URL "https://github.com/fmtlib/fmt/archive/${fmt_VERSION}.tar.gz")
-set(fmt_GIT "https://github.com/fmtlib/fmt")
-#set(fmt_MD5 "0cafa0aa67c0e28e9d79a2ebf8fc7732")
+set(fmt_VERSION "9.0.0")
+set(fmt_URL "https://github.com/fmtlib/fmt/archive/${fmt_VERSION}.tar.gz")
+set(fmt_MD5 "d56c8b0612b049bb1854f07c8b133f3c")
 set(fmt_CMAKE_ARGS
     -DFMT_DOC=OFF
     -DFMT_TEST=OFF
     -DCMAKE_POSITION_INDEPENDENT_CODE=ON
-    -DFMT_HEADER_ONLY=ON
     -DFMT_ARM_ABI_COMPATIBILITY=ON
 )
