92ec181c98f25e0775009614cfbf2f3be65eaf7a
Pierre-Antoine Lacaze <pal.dev@pa-l.fr>
2022-07-10 23:33:58 +0200

    date package: mute an erroneous warning

--- packages/date.cmake
+++ packages/date.cmake
@@ -4,3 +4,12 @@ set(date_CMAKE_ARGS
     -DUSE_SYSTEM_TZ_DB=ON
     -DBUILD_TZ_LIB=ON
 )
+
+macro(date_find name)
+    find_package(date ${ARGN})
+
+    # annoying compiler warning bug to disable
+    if (date_FOUND AND TARGET date::date)
+        target_compile_options(date::date INTERFACE $<$<CXX_COMPILER_ID:Clang,GNU>:-Wno-stringop-overflow>)
+    endif()
+endmacro()
