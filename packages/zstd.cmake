4dff54b951963ed6376f24280923c77cf4b90582
Pierre-Antoine Lacaze <pal.dev@pa-l.fr>
2022-07-10 23:35:50 +0200

    Update zstd package

--- packages/zstd.cmake
+++ packages/zstd.cmake
@@ -1,6 +1,7 @@
-set(zstd_VERSION "1.5.0")
-set(zstd_MD5 "a6eb7fb1f2c21fa80030a47993853e92")
+set(zstd_VERSION "1.5.2")
+set(zstd_MD5 "072b10f71f5820c24761a65f31f43e73")
 set(zstd_URL "https://github.com/facebook/zstd/releases/download/v${zstd_VERSION}/zstd-${zstd_VERSION}.tar.gz")
+
 set(zstd_CMAKE_ARGS
     -DZSTD_BUILD_PROGRAMS=OFF
     -DZSTD_LEGACY_SUPPORT=OFF
