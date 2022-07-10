deb8c4b659fc890ebff41c6a4c35eb24737b71b2
Pierre-Antoine Lacaze <pal.dev@pa-l.fr>
2022-07-10 23:33:36 +0200

    Eigen3: update to 3.4

--- packages/Eigen3.cmake
+++ packages/Eigen3.cmake
@@ -1,7 +1,5 @@
-#set(Eigen3_VERSION "3.3.7")
-#set(Eigen3_MD5 "05b1f7511c93980c385ebe11bd3c93fa")
-#set(Eigen3_URL "http://bitbucket.org/eigen/eigen/get/${Eigen3_VERSION}.tar.bz2")
 set(Eigen3_GIT "https://gitlab.com/libeigen/eigen")
+set(Eigen3_TAG 3.4)
 set(Eigen3_TAG "master")
 set(Eigen3_CMAKE_ARGS
     -DBUILD_TESTING=OFF
