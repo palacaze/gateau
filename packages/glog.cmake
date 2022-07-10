5a0c9b0a9e60c9e3be4bf871351a95be5c6fbe8a
Pierre-Antoine Lacaze <pal.dev@pa-l.fr>
2022-07-10 23:35:12 +0200

    glog: fix version to recent 0.6

--- packages/glog.cmake
+++ packages/glog.cmake
@@ -1,4 +1,7 @@
-set(glog_GIT "https://github.com/google/glog.git")
+set(glog_VERSION 0.6.0)
+set(glog_URL "https://github.com/google/glog/archive/refs/tags/v${glog_VERSION}.tar.gz")
+set(glog_MD5 "c98a6068bc9b8ad9cebaca625ca73aa2")
+# set(glog_GIT "https://github.com/google/glog.git")
 set(glog_CMAKE_ARGS
     -DWITH_GFLAGS=OFF
     -DWITH_UNWIND=OFF
