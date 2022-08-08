959c93289d84fba81adec45b71d71318dbcc4a5f
Pierre-Antoine Lacaze <pal.dev@pa-l.fr>
2022-08-08 19:31:59 +0200

    Enforce C++17 standard on Catch2
    It solves unresolved symbols with clang

--- packages/Catch2.cmake
+++ packages/Catch2.cmake
@@ -1,6 +1,7 @@
 set(Catch2_GIT "https://github.com/catchorg/Catch2.git")
 set(Catch2_TAG "devel")
 set(Catch2_CMAKE_ARGS
+    -DCMAKE_CXX_STANDARD=17
     -DCATCH_DEVELOPMENT_BUILD=OFF
     -DCATCH_INSTALL_DOCS=OFF
     -DCATCH_INSTALL_EXTRAS=ON
